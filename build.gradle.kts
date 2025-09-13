a04a1aff3daf2014db069027e58fa6170f0ded67
m8test <contact@m8test.com>
2025-09-13 22:41:16 +0800

    build: 更新m8test版本到0.1.22以及gradle版本到9.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,10 @@ plugins {
     alias(m8test.plugins.kotlin.android) apply false
     alias(m8test.plugins.kotlin.compose) apply false
 }
+
+buildscript {
+    dependencies {
+        // Add the Maven coordinates and latest version of the plugin
+        classpath(m8test.m8test.gradle)
+    }
+}
